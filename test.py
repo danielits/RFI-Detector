d5797023ae5e828ee7adbb2ee1c272e85a650565
danielits <daniel.bravo.lib@gmail.com>
2021-04-05 15:20:37 -0400

    updated vcs after caladigital was removed

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 a = np.random.randint(0, 100, 2**10)
-b = np.random.randint(0, 2 * np.pi, 2**10)
-
+b = np.random.randint(0, 200 * np.pi, 2**10)
+b = b / 100
 
 real = a * np.cos(b)
 imag = a * np.sin(b)
